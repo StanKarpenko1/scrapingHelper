3ed6728a1c0c7fe3ac3fa85efcebacd79e3bc0bb
sky <sky@storis.com>
2024-08-22 10:49:28 -0400

    misc up

--- helper/failedTests.js
+++ helper/failedTests.js
@@ -3,36 +3,36 @@ import puppeteer from 'puppeteer';
 
 
 (async () => {
-  
+
 
     const urlToFile = `
         file:///C:/Users/sky/Downloads/index.html
     
     `;
-  
+
 
     // open browser
     const browser = await puppeteer.launch({
-        headless: false, 
+        headless: false,
         defaultViewport: false,
     });
     const page = await browser.newPage();
     await page.goto(urlToFile.trim());
 
-     //start the test
+    //start the test
     await page.waitForSelector('button.test--header-btn---mI0Oy');
 
     const itElements = await page.$$eval('button.test--header-btn---mI0Oy', buttons => {
         return buttons.map(button => {
-        const failIconElement = button.querySelector('i.material-icons.md-18.test--icon---2jgH_.test--fail---3u2w0');
-        if (failIconElement) {
-            const titleElement = button.querySelector('h4.test--title---4c0rg');
-            if (titleElement) {
-            return titleElement.textContent;
+            const failIconElement = button.querySelector('i.material-icons.md-18.test--icon---2jgH_.test--fail---3u2w0');
+            if (failIconElement) {
+                const titleElement = button.querySelector('h4.test--title---4c0rg');
+                if (titleElement) {
+                    return titleElement.textContent;
+                }
             }
-        }
-        return null;
-        }).filter(title => title !== null); 
+            return null;
+        }).filter(title => title !== null);
     });
 
     // print out results
@@ -41,5 +41,5 @@ import puppeteer from 'puppeteer';
     // shut down the browser
     await new Promise(r => setTimeout(r, 5000));
     await browser.close();
-     
+
 })();
